3fb15ec811ee3a7ce11703e2d422cefa89da275c
Nick Balandin <balandin.nick@gmail.com>
2020-05-17 13:32:06 +0300

    Test heroku.

--- labor_exchange/settings.py
+++ labor_exchange/settings.py
@@ -100,7 +100,7 @@ USE_L10N = True
 USE_TZ = True
 # endregion
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
