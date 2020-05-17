aadf0d81a31079a9acff6fbbebd4b04f20fb3b1e
Nick Balandin <balandin.nick@gmail.com>
2020-05-17 13:01:01 +0300

    Try to give images.

--- labor_exchange/urls.py
+++ labor_exchange/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
-from labor_exchange.settings import DEBUG, MEDIA_ROOT, MEDIA_URL
+from labor_exchange.settings import DEBUG, MEDIA_ROOT, MEDIA_URL, STATIC_ROOT, STATIC_URL
 
 from .views import HomeView
 
@@ -17,4 +17,5 @@ urlpatterns = [
 ]
 
 if DEBUG:
+    urlpatterns += static(STATIC_URL, document_root=STATIC_ROOT)
     urlpatterns += static(MEDIA_URL, document_root=MEDIA_ROOT)
